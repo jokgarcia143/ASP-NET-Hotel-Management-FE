21675380586c8ebf49ff024810c0a17557605d84
jokgarcia <33973877+jokgarcia@users.noreply.github.com>
2020-04-18 21:29:28 +0800

    Initial Code

---
+++ HMS/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "Le Simone International Resort";
+        public const string HotelShortName = "Le Simone";
+        public const string HotelNameAbbreviate = "SIR";
+    }
+}
